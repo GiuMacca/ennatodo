1e50d03b9f8a5eaf09756cc400742547292dae5a
GiuMacca <80456700+GiuMacca@users.noreply.github.com>
2026-05-21 16:55:28 +0200

    feat: EnterPasswordPage (enter_password.dart)

---
+++ lib/presentation/auth/pages/enter_password.dart
@@ -0,0 +1,67 @@
+import 'package:ennatodo/common/widgets/buttons/basic_button.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class EnterPasswordPage extends StatelessWidget {
+  const EnterPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _siginText(context),
+            const SizedBox(height: 20),
+            _passwordField(context),
+            const SizedBox(height: 20),
+            _continueButton(),
+            const SizedBox(height: 20),
+            _forgotPassword(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _siginText(BuildContext context) {
+    return const Text(
+      'Sign In',
+      style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(decoration: InputDecoration(hintText: 'Enter Password'));
+  }
+
+  Widget _continueButton() {
+    return BasicButton(
+      onPressed: (){},
+      title: 'Continue',
+    );
+  }
+
+  Widget _forgotPassword(BuildContext context){
+    return RichText(  // Per Click
+      text: TextSpan(
+        children: [
+           TextSpan(
+            text: 'Forgot password? '
+           ),
+            TextSpan(
+            text: 'Reset',
+            recognizer: TapGestureRecognizer()..onTap = () {
+              // signup page soon
+            },
+            style: TextStyle(
+              fontWeight: FontWeight.bold
+            )
+           )
+        ]
+      )
+    );
+  }
+}
